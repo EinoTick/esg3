b2fe59f8642b1225cc18bd22cdf93fbcc766fbe3
Eino Tick <einotick@yahoo.com>
2025-02-13 15:00:52 +0200

    init react project

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section className="bg-gray-100 py-20">
+            <div className="container mx-auto text-center">
+                <h1 className="text-5xl font-bold text-gray-800 leading-tight mb-6">
+                    Welcome to Our Website
+                </h1>
+                <p className="text-xl text-gray-600 mb-10">
+                    Your friendly neighborhood resource for all things related to... (Your website's description)
+                </p>
+                <div className="flex justify-center"> {/* Center the buttons */}
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg mr-4">
+                        Get Started
+                    </button>
+                    <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-3 px-6 rounded-lg">
+                        Learn More
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <nav className="bg-gray-800 p-4">
+            <div className="container mx-auto flex flex-wrap items-center justify-between">
+                <a href="/" className="flex items-center">
+                    <span className="text-white font-bold text-xl">My Website</span> {/* Replace with your logo/brand */}
+                </a>
+
+                <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 text-white rounded-md lg:hidden focus:outline-none focus:ring-2 focus:ring-gray-200">
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+                    </svg>
+                </button>
+
+                <div className={`${isMobileMenuOpen ? 'block' : 'hidden'} lg:block lg:flex lg:items-center lg:w-auto w-full`} id="mobile-menu">
+                    <ul className="flex flex-col mt-4 lg:flex-row lg:justify-end lg:space-x-8 lg:mt-0 lg:text-sm lg:font-medium">
+                        <li>
+                            <a href="/" className="block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:p-0 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about" className="block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:p-0 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white">About</a>
+                        </li>
+                        <li>
+                            <a href="/services" className="block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:p-0 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white">Services</a>
+                        </li>
+                        <li>
+                            <a href="/contact" className="block py-2 pl-3 pr-4 text-white rounded lg:bg-transparent lg:p-0 hover:bg-gray-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-white">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
